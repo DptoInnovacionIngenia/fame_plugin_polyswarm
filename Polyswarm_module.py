60c34cf5c97b8ee3fadc16ba654b42e86e06ba1c
DptoInnovacionIngenia <67777725+DptoInnovacionIngenia@users.noreply.github.com>
2021-08-31 10:58:01 +0200

    Update Polyswarm_module.py

--- Polyswarm_module.py
+++ Polyswarm_module.py
@@ -36,37 +36,34 @@ class Polyswarm_module(ProcessingModule):
         poly = PolyswarmAPI(key=self.api_key)
 
         if file_type == 'url' or file_type == 'msg' or file_type == 'eml':
-	        pass
-	    else:
-            positives = 0
-            total = 0
-            sha256 = None
-            if file_type == "hash":
-                sha256 = target.lower()
-            else:
-                sha256 = hashlib.sha256(open(target, 'r').read()).hexdigest()
-            try:
-                response = poly.search(sha256)
-                self.results['scans'] = list()
-                for result in response:
-                    if result.assertions:
-                        for assertion in result.assertions:
-                            if assertion.verdict:
-                                self.results['scans'].append({'av': assertion.author_name, 'veredict': 'Malware'})
-                                positives += 1
-                            total += 1
-
-                        self.results['total'] = "{0}/{1}".format(positives, total)
-                        self.results['positives'] = positives
-                        self.results['PolyScore'] = result.polyscore
-                        self.results['permalink'] = result.permalink
-                        self.results['message'] = "Hash found"
-                        return True
-                    else:
-                        return False
-            except NoResultsException:
-                return False
-            except Exception as error:
-                return False
-            finally:
-                return True
+		pass
+	else:
+		positives = 0
+		total = 0
+		sha256 = None
+		if file_type == "hash":
+			sha256 = target.lower()
+		else:
+			sha256 = hashlib.sha256(open(target, 'r').read()).hexdigest()
+			try:
+				response = poly.search(sha256)
+				self.results['scans'] = list()
+				for result in response:
+					if result.assertions:
+						for assertion in result.assertions:
+							if assertion.verdict:
+								self.results['scans'].append({'av': assertion.author_name, 'veredict': 'Malware'})
+								positives += 1
+							total += 1
+						self.results['total'] = "{0}/{1}".format(positives, total)
+						self.results['positives'] = positives
+						self.results['PolyScore'] = result.polyscore
+						self.results['permalink'] = result.permalink
+						self.results['message'] = "Hash found"
+						return True
+					else:
+						return False
+			except NoResultsException:
+				return False
+			except Exception as error:
+				return False
